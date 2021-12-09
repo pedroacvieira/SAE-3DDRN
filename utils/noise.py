20fbb492a99ced0cfc35bb745c7d81760bb09362
Pedro Vieira <pedrovieira@arrowplan.com>
2021-12-09 18:08:39 +0100

    Implement pixel-wise additive and multiplicative gaussian noise

--- utils/noise.py
+++ utils/noise.py
@@ -18,27 +18,39 @@ IMAGE_NAME = 'proc_image.pth'
 # Set noise parameters
 NOISE_TYPE = 'salt_and_pepper'
 SIGNAL_NOISE_RATIO = 0.9
+MAX = 1.0
 
 
-def add_noise(img, noise_amount, noise_type='salt_and_pepper'):
+def add_noise(img, noise_param, noise_type='salt_and_pepper'):
     out = np.copy(img)
     pixels_per_band = img.shape[0] * img.shape[1]
 
+    # Applies a salt and pepper noise to each band for a noise_param ratio of the pixels
+    # noise_param = fraction of noisy pixels [0.1)
     if noise_type is 'salt_and_pepper':
         # Calculate the number of image salt and pepper noise points
-        num_points = int(pixels_per_band * noise_amount)
+        num_points = np.floor(pixels_per_band * noise_param)
 
         for band in range(img.shape[2]):
             for i in range(num_points):
                 rand_x = np.random.randint(img.shape[0])  # Generate random x coordinate
                 rand_y = np.random.randint(img.shape[1])  # Generate random y coordinate
 
-                if np.random.random() <= 0.5:
-                    out[rand_x, rand_y, band] = 0
-                else:
-                    out[rand_x, rand_y, band] = 1
-    elif noise_type is 'gaussian':
-        print('Implement it')
+                out[rand_x, rand_y, band] = 0 if np.random.random() <= 0.5 else MAX
+
+    # Applies an additive gaussian noise to every pixel with mean and variance defined by noise_param
+    # noise_param = [mu, sigma]
+    elif noise_type is 'additive_gaussian':
+        for idx, element in np.ndenumerate(out):
+            noise = np.random.normal(0.0, noise_param)
+            out[idx] = np.min(MAX, element + noise)
+
+    # Applies a multiplicative gaussian noise to every pixel with mean and variance defined by noise_param
+    # noise_param = sigma; noise = normal(1.0, noise_param)
+    elif noise_type is 'multiplicative_gaussian':
+        for idx, element in np.ndenumerate(out):
+            noise = np.random.normal(1.0, noise_param)
+            out[idx] = np.min(MAX, element + noise)
     else:
         raise Exception('noise type not implemented')
 
