c0667aed324b21252d30b0202a09e4b96cde4a3e
Pedro Vieira <pedrovieira@arrowplan.com>
2021-12-14 13:38:10 +0100

    Add possibility of extracting the per class accuracy from each model as well

--- utils/extract_results.py
+++ utils/extract_results.py
@@ -9,10 +9,13 @@ import numpy as np
 PATH = '../../../Results/'
 EXPERIMENT = 'full/'
 FILE = 'test_paviau_dffn.txt'
-DATASETS = ['paviau', 'indian_pines', 'salinas']
+# DATASETS = ['paviau', 'indian_pines', 'salinas']  # Use this for all datasets
+DATASETS = ['paviau']
 NETWORKS = ['sdmm', 'dffn', 'vscnn', 'sae3ddrn']
 
 VALUE_POSITION = 3
+NUM_RUNS = 10
+PRINT_PER_CLASS_ACCURACY = True
 
 
 # Get test results from text file
@@ -20,6 +23,7 @@ def get_values(filename):
     overall_accuracy = []
     average_accuracy = []
     kappa_coefficients = []
+    per_class_accuracy = []
 
     with open(filename, 'r') as file:
         line = file.readline()
@@ -36,17 +40,29 @@ def get_values(filename):
             elif 'KAPPA COEFFICIENT' in line:
                 words = line.split(' ')
                 kappa_coefficients.append(float(words[VALUE_POSITION]))
+            elif 'PER CLASS ACCURACY' in line:
+                run_class_acc = []
+                line = file.readline()
+                while line != '\n':
+                    # line = line[1:] if line[0] == ' ' else line  # Remove space in the beginning of a line
+                    run_class_acc.extend(line.strip('[]\n').split(' '))
+                    line = file.readline()
+                non_empty = [float(x) for x in run_class_acc if x]
+                per_class_accuracy.append([float(x) for x in run_class_acc if x])
 
             # Get next line
             line = file.readline()
 
-    assert len(overall_accuracy) == len(average_accuracy), 'Wrong list lengths! [1]'
-    assert len(average_accuracy) == len(kappa_coefficients), 'Wrong list lengths! [2]'
+    assert len(overall_accuracy) == NUM_RUNS, f'File should have {NUM_RUNS} runs! [1]'
+    assert len(average_accuracy) == NUM_RUNS, f'File should have {NUM_RUNS} runs! [2]'
+    assert len(kappa_coefficients) == NUM_RUNS, f'File should have {NUM_RUNS} runs! [3]'
+    assert len(per_class_accuracy) == NUM_RUNS, f'File should have {NUM_RUNS} runs! [4]'
 
     oa = np.array(overall_accuracy)
     aa = np.array(average_accuracy)
     kappa = np.array(kappa_coefficients)
-    return oa, aa, kappa
+    class_acc = np.array(per_class_accuracy)
+    return oa, aa, kappa, class_acc
 
 
 # Main for running script independently
@@ -55,20 +71,19 @@ def main():
         for net in NETWORKS:
             file = 'test_' + data + '_' + net + '.txt'
             filename = PATH + EXPERIMENT + file
-            oa, aa, kappa = get_values(filename)
-
-            oa_mean = oa.mean()
-            aa_mean = aa.mean()
-            kp_mean = kappa.mean()
+            oa, aa, kappa, class_acc = get_values(filename)
 
             print(f'TEST: {net} with {data}')
             print('#' * 15)
-            print(f'OA: {oa.mean():.6f} (+- {oa.std():.6f})')
-            print(f'AA: {aa.mean():.6f} (+- {aa.std():.6f})')
-            print(f'Kappa: {kappa.mean():.6f} (+- {kappa.std():.6f})')
+            print(f'OA: {oa.mean():.6f} ({oa.std():.6f})')
+            print(f'AA: {aa.mean():.6f} ({aa.std():.6f})')
+            print(f'Kappa: {kappa.mean():.6f} ({kappa.std():.6f})')
             print('-' * 15)
             print(f'Max OA: {np.max(oa):.5f}')
             print(f'Min OA: {np.min(oa):.5f}')
+            if PRINT_PER_CLASS_ACCURACY:
+                print('-' * 15)
+                print(f'Per Class Accuracy: {np.mean(class_acc, axis=0)}')
             print('')
 
 
